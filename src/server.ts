a876024b1c9bee54f4f4ecfaf46e39ebbd631b4f
Matheus8174 <souzamatheus8174@gmail.com>
2021-02-22 23:29:10 -0300

    chore: add git-commit-msg-linter

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,16 @@ import express from 'express'
 
 const app = express()
 
+app.post('/', (req, res) => {
+  return res.json({
+    message: 'Post'
+  })
+})
+
+app.get('/', (req, res) => {
+  return res.json({
+    message: 'Get'
+  })
+})
+
 app.listen(3000, () => console.log('server run'))
