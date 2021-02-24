7b849eac1c516f0c10f37baf954c7e10ea6f6323
Matheus8174 <souzamatheus8174@gmail.com>
2021-02-24 12:47:26 -0300

    feat: create User model

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import User from '../models/User';
 
 class UserController {
   async create(req: Request, res: Response) {
     const { body } = req;
-    console.log(body);
-    return res.send();
+
+    const userRepository = getRepository(User);
+
+    const user = userRepository.create();
   }
 }
 
