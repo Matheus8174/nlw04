6e5cb9d826f681cd4ffd764ecd298fd552cc16ab
Matheus8174 <souzamatheus8174@gmail.com>
2021-02-26 13:19:10 -0300

    refactor: update user.test.ts

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,9 +1,18 @@
 import request from 'supertest';
 import app from '../app';
 
+import createConnection from '../database';
+
 describe('Users', () => {
-  request(app).post('/users').send({
-    email: 'user@email.com',
-    name: 'user example'
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it('Should be able to create a new user', async () => {
+    const response = await request(app).post('/users').send({
+      email: 'user@email.com',
+      name: 'user example'
+    });
   });
 });
--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,9 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
+  /* cria opções de conexão com o banco de dados diferentes
+     para o ambiente de teste e o ambiente de dev/produção
+  */
   return createConnection(
     Object.assign(defaultOptions, {
       database:
