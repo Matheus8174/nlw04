9a025d33974c7565f817e9378c3cab0b4de57118
Matheus8174 <souzamatheus8174@gmail.com>
2021-02-27 01:04:21 -0300

    build: update package.json

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,6 +1,5 @@
 import request from 'supertest';
 import app from '../app';
-
 import createConnection from '../database';
 
 describe('Users', () => {
@@ -11,8 +10,14 @@ describe('Users', () => {
 
   it('Should be able to create a new user', async () => {
     const response = await request(app).post('/users').send({
-      email: 'user@email.com',
-      name: 'user example'
+      email: 'usr@mal.o',
+      name: 'userexmp'
     });
+
+    expect(response.status).toBe(201);
   });
 });
+
+// 1:01:45
+
+// right now time, 1:01:43
